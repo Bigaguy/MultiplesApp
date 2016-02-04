90f3feed262ccefa872b6005d4b57eae29ccfe4b
Andrew Ingram <andrewingram@Andrews-MacBook-Pro.local>
2016-02-03 21:53:17 -0500

    First Commit

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,17 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var multiple = 0
+    var maxNumber = 1000
+    var firstNumber = 0
+    var currentNumber = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var logoTitle: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var labelAdd: UILabel!
+    @IBOutlet weak var textMultiple: UITextField!
+    
+    @IBAction func playButton(sender: AnyObject) {
+        
+        if textMultiple.text != nil && textMultiple.text != "" {
+        logoTitle.hidden=true
+        labelAdd.hidden=false
+        playButton.hidden=true
+        addButton.hidden=false
+        textMultiple.hidden=true
+        multiple=Int(textMultiple.text!)!
+       
+        }else{
+            textMultiple.text="Enter Value"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateLabel(){
+        labelAdd.text="\(firstNumber) + \(multiple) = \(currentNumber)"
+        
     }
-
-
+    
+    
+    
+    @IBAction func addButton(sender: AnyObject) {
+        if currentNumber<=maxNumber {
+            updateLabel()
+            currentNumber=firstNumber+multiple
+            firstNumber+=multiple
+            
+        }
+        
+        
+    }
+    
+        
 }
+   
+
+
 
